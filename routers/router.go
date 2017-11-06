d3b39da63c0a2ef12559a0180e9b590b1c77d6b0
Steven Lu <steven.lu@network-box.cn>
2017-11-06 10:00:05 +0800

    router.go

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,35 @@
 package routers
 
 import (
-	"testblog/controllers"
 	"github.com/astaxie/beego"
+	"blog/controllers/admin"
+        "blog/controllers/blog"
+        "blog/controllers/login"
 )
 
 func init() {
+        beego.Router("/login", &login.LoginController{})
+    beego.Router("/logout", &login.LoginController{}, "*:Logout")
+
     beego.Router("/", &controllers.MainController{})
+    beego.Router("/page/?:page", &blog.MainController{})
+    beego.Router("/show/:id", &blog.ShowController{})
+
+    ns :=
+    beego.NewNamespace("/admin",
+            beego.NSRouter("/", &admin.IndexController{}),
+            beego.NSNamespace("/article",
+                   beego.NSRouter("/", &admin.ArticleController{}),
+                   beego.NSRouter("/add", &admin.ArticleController{},"*:AddArticle"),
+                   beego.NSRouter("/edit/:id", &admin.ArticleController{},"*:EditArticle"),
+                   beego.NSRouter("/delete/:id", &admin.ArticleController{},"*:DelArticle")
+	    ),
+	    beego.NSNamespace("/user",
+	           beego.NSRouter("/", &admin.UserController{}),
+		   beego.NSRouter("/setting", &admin.UserController{},"*:EditUser"),
+		   beego.NSRouter("/changepassword", &admin.UserController{},"*:ChangePassword"),
+
+	    ),
+     )
+     beego.AddNamespace(ns)
 }
